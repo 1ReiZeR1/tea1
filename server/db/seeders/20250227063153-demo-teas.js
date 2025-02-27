d48361941cdf2c682aa76b88c380ea52f03aa5c6
Shannanaa <jannaspasyonkina@gmail.com>
2025-02-27 20:48:42 +1000

    tea page done plus image and teas

--- server/db/seeders/20250227063153-demo-teas.js
+++ server/db/seeders/20250227063153-demo-teas.js
@@ -139,6 +139,60 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      {
+        name: 'Цейлонский чай (Димбула)',
+        cultivationPlace: 'Шри-Ланка, Димбула',
+        image: 'https://avatars.mds.yandex.net/i?id=2328e6adddeb989bbb11529719d6af49_l-6446516-images-thumbs&n=13',
+        description: 'Чёрный чай с ярким и освежающим вкусом, часто с нотками цитрусовых.',
+        userId:1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Кандийский чай',
+        cultivationPlace: 'Шри-Ланка, Канди',
+        image: 'https://avatars.mds.yandex.net/i?id=d798df9804fdacdf9f6b9ea463e8fc63_l-5319478-images-thumbs&n=13',
+        description: 'Чёрный чай средней крепости с насыщенным вкусом и медным оттенком.',
+        userId:1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Рухуна',
+        cultivationPlace: 'Шри-Ланка, Рухуна',
+        image: 'https://content.onliner.by/service_market/9cff6695/2100x1200/5bb3a75e0c9d9b0237e3026759ad76a1.jpeg',
+        description: 'Чёрный чай с тёмным настоем и уникальным солодовым вкусом.',
+        userId:1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Афганский Зелёный Чай',
+        cultivationPlace: 'Афганистан, Нуристан',
+        image: 'https://avatars.mds.yandex.net/get-mpic/5281727/img_id1521244307261704822.jpeg/orig',
+        description: 'Зелёный чай, часто заваривается с кардамоном и другими специями.',
+        userId:1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Бразильский Чёрный Чай',
+        cultivationPlace: 'Бразилия, Сан-Паулу',
+        image: 'https://main-cdn.sbermegamarket.ru/big2/hlr-system/151/407/729/263/127/600018584470b1.jpg',
+        description: 'Чёрный чай с мягким и сладковатым вкусом, хорошо сочетается с молоком.',
+        userId:1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Бразильский Зелёный Чай',
+        cultivationPlace: 'Бразилия, Парана',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Twinings_gunpowder_green_tea.jpg',
+        description: 'Зелёный чай с травянистым вкусом и лёгкой горчинкой.',
+        userId:1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
 
 
     ], {});
